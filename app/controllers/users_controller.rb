7766c4a8cd362857c5e9aa1036da57c3ba6d5e47
Fabien Loup <info@norydev.com>
2015-02-23 16:23:59 +0100

    aded user view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
   def show
   end
 
@@ -9,5 +11,18 @@ class UsersController < ApplicationController
   end
 
   def destroy
+    @user.destroy
+    redirect_to root_path, notice: 'Your account has been successfully removed. We have no data about you anymore.'
   end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def user_params
+      params.require(:user).permit(:first_name, :last_name)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  resources :users, only: [:show, :edit, :update, :destroy]
-
   resources :bookings
 
   resources :flats
@@ -9,6 +7,8 @@ Rails.application.routes.draw do
   get 'welcome/index'
 
   devise_for :users
+
+  resources :users, only: [:show, :edit, :update, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
