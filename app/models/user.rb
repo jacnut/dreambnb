fe3b188daa90f295a1cdaa10d2a78732567102b1
Fabien Loup <info@norydev.com>
2015-02-23 19:01:18 +0100

    add picture to profile and links to flats

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,10 @@ class User < ActiveRecord::Base
 
   has_many :flats
   has_many :bookings
+
+  has_attached_file :avatar,
+    styles: { big: "600x600#", medium: "300x300#", thumb: "100x100#" }
+
+  validates_attachment_content_type :avatar,
+    content_type: /\Aimage\/.*\z/
 end
