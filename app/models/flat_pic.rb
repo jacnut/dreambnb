8e7ec3aa0a52e077868aa6bdab3cb36ff58e09f9
Jacques De Liedekerke <jacnut@gmail.com>
2015-02-25 13:13:46 +0100

    flats layout + general layout

--- app/models/flat_pic.rb
+++ app/models/flat_pic.rb
@@ -1,6 +1,6 @@
 class FlatPic < ActiveRecord::Base
     has_attached_file :picture,
-    styles: { medium: "300x300>", thumb: "100x100>" }
+    styles: { medium: "300x200#", thumb: "100x100>" }
 
   validates_attachment_content_type :picture,
     content_type: /\Aimage\/.*\z/
