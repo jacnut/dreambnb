5486c9fd8994ad3bc5a324b35227fb9673f44aa0
Renaat De Vos <renaat@drvn.be>
2015-02-25 15:22:35 +0100

    booking functionality working

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   cities = City.create([( name: 'Chicago' ), ( name: 'Copenhagen' )])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Flat.create(
+  user_id: 1,
+  name: 'Le bon lieu',
   city: 'Brussels',
   accomodates: '3',
   price: '65 EUR per night',
@@ -14,6 +16,8 @@ Flat.create(
 )
 
 Flat.create(
+  user_id: 2,
+  name: 'Au coeur de ville',
   city: 'Paris',
   accomodates: '2',
   price: '125 EUR per night',
@@ -22,6 +26,8 @@ Flat.create(
 )
 
 Flat.create(
+  user_id: 3,
+  name: 'The Bricks',
   city: 'London',
   accomodates: '4',
   price: '235 EUR per night',
@@ -30,6 +36,8 @@ Flat.create(
 )
 
 Flat.create(
+  user_id: 2,
+  name: 'Haile Selassie',
   city: 'Addis Abebea',
   accomodates: '2',
   price: '35 EUR per night',
@@ -38,6 +46,8 @@ Flat.create(
 )
 
 Flat.create(
+  user_id: 1,
+  name: 'Brussels Delight',
   city: 'Brussels',
   accomodates: '2',
   price: '95 EUR per night',
@@ -46,6 +56,8 @@ Flat.create(
 )
 
 Flat.create(
+  user_id: 3,
+  name: 'Au tour Eiffel',
   city: 'Paris',
   accomodates: '3',
   price: '105 EUR per night',
@@ -54,6 +66,8 @@ Flat.create(
 )
 
 Flat.create(
+  user_id: 1,
+  name: 'The Tower',
   city: 'London',
   accomodates: '8',
   price: '760 EUR per night',
@@ -62,6 +76,8 @@ Flat.create(
 )
 
 Flat.create(
+  user_id: 3,
+  name: 'Tizeze',
   city: 'Addis Abebea',
   accomodates: '4',
   price: '95 EUR per night',
@@ -70,6 +86,8 @@ Flat.create(
 )
 
 Flat.create(
+  user_id: 2,
+  name: 'Chez Fabien',
   city: 'Brussels',
   accomodates: '4',
   price: '105 EUR per night',
@@ -78,6 +96,8 @@ Flat.create(
 )
 
 Flat.create(
+  user_id: 3,
+  name: 'Chez Jaques',
   city: 'Brussels',
   accomodates: '2',
   price: '125 EUR per night',
@@ -86,6 +106,8 @@ Flat.create(
 )
 
 Flat.create(
+  user_id: 3,
+  name: 'Fox Place',
   city: 'London',
   accomodates: '1',
   price: '65 EUR per night',
@@ -94,6 +116,8 @@ Flat.create(
 )
 
 Flat.create(
+  user_id: 3,
+  name: 'The Lions Den',
   city: 'Addis Abebea',
   accomodates: '2',
   price: '75 EUR per night',
