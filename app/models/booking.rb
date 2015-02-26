43dcedac20d023da0a087b4365593d7091d5e98c
Jacques De Liedekerke <jacnut@gmail.com>
2015-02-26 17:15:26 +0100

    need to check reload on new booking and edit booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,7 +3,7 @@ class Booking < ActiveRecord::Base
   belongs_to :user
 
   validates :user_id, :flat_id, :start_date, :end_date, presence: true
-  validates :date_must_be_in_future, inclusion: { in: [true], message: "date can't be in the past" }
+  validates :date_must_be_in_future, inclusion: { in: [true]}
 
    def date_must_be_in_future
     start_date > Date.today
