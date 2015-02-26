c4573a8965695068fffcbbc11dfbd2348c159145
Fabien Loup <info@norydev.com>
2015-02-26 11:04:18 +0100

    fix jacque's mess

--- db/migrate/20150226095009_remove_coordinates_to_flats.rb
+++
@@ -1,7 +0,0 @@
-class RemoveCoordinatesToFlats < ActiveRecord::Migration
-  def change
-    remove_column :flats, :latitude, :float
-    remove_column :flats, :longitude, :float
-    remove_column :flats, :address, :string
-  end
-end
