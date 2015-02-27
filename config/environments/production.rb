e3e1e233f634947179bf5ebc3e59e3ed45e4c957
Fabien Loup <info@norydev.com>
2015-02-27 10:22:14 +0100

    added mandrill for mailing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,17 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  ActionMailer::Base.smtp_settings = {
+    :port =>           '587',
+    :address =>        'smtp.mandrillapp.com',
+    :user_name =>      ENV['MANDRILL_USERNAME'],
+    :password =>       ENV['MANDRILL_APIKEY'],
+    :domain =>         'heroku.com',
+    :authentication => :plain
+  }
+
+  ActionMailer::Base.delivery_method = :smtp
+
   config.action_mailer.default_url_options = { host: 'http://dreambnb.herokuapp.com' }
 
   # Code is not reloaded between requests.
